4bcc3a35a9df2fa10808304705cbeb72f7b4b8b0
pelsan <pelsanmexico@gmail.com>
2021-07-14 17:36:54 -0500

    feat: adding source of otumba package

---
+++ otumba_package/otumba/README.md
@@ -0,0 +1,8 @@
+# otumba package
+Summary of the package
+
+# Files
+
+Explanation od files in the package
+
+# Installation
---
+++ otumba_package/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='otumba',
+      version='0.2',
+      description='AI on Cloud Distributed Systems',
+      packages=['otumba'],
+      author = 'Pedro Coronel Santeliz',
+      author_email = 'pelsanmexico@gmail.com',
+      zip_safe=False)
